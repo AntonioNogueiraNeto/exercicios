96e18aca0aa2793821927e8235646334c71be1af
Antonio <antonio.nogueiraneto@icloud.com>
2023-01-31 16:27:57 -0300

    adicionadno exercicio 6.d

--- modulo-nodejs/dia1/exercicio6/simpsons.js
+++ modulo-nodejs/dia1/exercicio6/simpsons.js
@@ -1,42 +1,50 @@
-const fs = require('fs').promises
+const fs = require("fs").promises;
 
-async function lerTodosPersonagens(){
-   const conteudoDoArquivo = await fs.readFile('./simpsons.json', 'utf-8')
-    const simpsons = JSON.parse(conteudoDoArquivo)
-    const strings = simpsons.map(({id, name}) => `${id} :  ${name}`)
-    strings.forEach((strings) => console.log(strings))
+async function lerTodosPersonagens() {
+  const conteudoDoArquivo = await fs.readFile("./simpsons.json", "utf-8");
+  const simpsons = JSON.parse(conteudoDoArquivo);
+  const strings = simpsons.map(({ id, name }) => `${id} :  ${name}`);
+  strings.forEach((strings) => console.log(strings));
 }
 
 async function buscarPeloId(id) {
-    const filecontent = await fs.readFile('./simpsons.json', 'utf-8');
-    const simpsons = JSON.parse(filecontent)
-    const simpsonsFind = simpsons.find((simpson) => Number(simpson.id)=== id)
-   
-    if(!simpsonsFind) {
-        throw new Error('id não encontrado')
-    }
+  const filecontent = await fs.readFile("./simpsons.json", "utf-8");
+  const simpsons = JSON.parse(filecontent);
+  const simpsonsFind = simpsons.find((simpson) => Number(simpson.id) === id);
 
-    return simpsonsFind
+  if (!simpsonsFind) {
+    throw new Error("id não encontrado");
+  }
 
+  return simpsonsFind;
 }
 
-
-
-
-async function filterSimpsons(){
-    
-    const filecontent = await fs.readFile('./simpsons.json', 'utf-8')
-    const simpsons = JSON.parse(filecontent);
-    const newArray = simpsons.filter((simpson) => simpson.id !== '10' && simpson.id !==  '6')
-    await fs.writeFile('./simpsons.json', JSON.stringify(newArray));
+async function filterSimpsons() {
+  const filecontent = await fs.readFile("./simpsons.json", "utf-8");
+  const simpsons = JSON.parse(filecontent);
+  const newArray = simpsons.filter(
+    (simpson) => simpson.id !== "10" && simpson.id !== "6"
+  );
+  await fs.writeFile("./simpsons.json", JSON.stringify(newArray));
 }
 
+async function createSimpsonsFamily() {
+  const filecontent = await fs.readFile("./simpsons.json", "utf-8");
+  const simpsons = JSON.parse(filecontent);
 
+  const familyids = [1, 2, 3, 4];
 
-  function main(){
-    //lerTodosPersonagens();
-    buscarPeloId(3).then((simpson) => console.log(simpson))
-    filterSimpsons();
+  const simpsonsFamily = simpsons.filter((simpson) =>
+    familyids.includes(Number(simpson.id))
+  );
+  await fs.writeFile('./simpsonsFamily.json', JSON.stringify(simpsonsFamily));
 }
 
-main()
+function main() {
+  //lerTodosPersonagens();
+  buscarPeloId(3).then((simpson) => console.log(simpson));
+  filterSimpsons();
+  createSimpsonsFamily();
+}
+
+main();
